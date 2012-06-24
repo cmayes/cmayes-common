626f47541d5ccd6920873c36c0b6ca6ecf74fcc1
Christopher Mayes (cmayes) <Christopher.Mayes@cmegroup.com>
2012-06-24 11:41:56 -0500

    Added Guava 12 to deps. Added extension filter.

---
+++ src/main/java/com/cmayes/common/file/ExtensionFilter.java
@@ -0,0 +1,82 @@
+package com.cmayes.common.file;
+
+import static com.cmayes.common.exception.ExceptionUtils.asNotNull;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+
+/**
+ * Filters filenames by extension.
+ */
+public class ExtensionFilter implements FilenameFilter {
+
+    /** List of extensions to filter on. */
+    private final Collection<String> extList;
+
+    private final Pattern extPat;
+
+    /**
+     * Creates a file filter that selects files with the given format list. Be
+     * sure to include any needed prefixes (a dot, etc.).
+     * 
+     * @param format
+     *            The format to filter for.
+     */
+    public ExtensionFilter(Collection<String> exts) {
+        this.extList = Collections.unmodifiableCollection(asNotNull(exts,
+                "Extensions are null"));
+        Collection<String> escExts = Collections2.transform(extList,
+                new Function<String, String>() {
+                    public String apply(String input) {
+                        return Pattern.quote(input);
+                    }
+                });
+        extPat = Pattern.compile(String.format(".*(%s)$",
+                StringUtils.join(escExts, "|")));
+    }
+
+    /**
+     * Creates a file filter that selects files with the given format list. Be
+     * sure to include any needed prefixes (a dot, etc.).
+     * 
+     * @param format
+     *            The format to filter for.
+     */
+    public ExtensionFilter(String... exts) {
+        this(Arrays.asList(exts));
+    }
+
+    /**
+     * Tests if a specified file should be included in a file list.
+     * 
+     * @param dir
+     *            the directory in which the file was found.
+     * @param name
+     *            the name of the file.
+     * @return <code>true</code> if and only if the name should be included in
+     *         the file list; <code>false</code> otherwise.
+     * @see java.io.FilenameFilter#accept(java.io.File, java.lang.String)
+     */
+    public boolean accept(File dir, String name) {
+        return extPat.matcher(name).matches();
+    }
+
+    /**
+     * Returns a read-only view of the extensions that are included in this
+     * filter.
+     * 
+     * @return The extensions allowed by this filter.
+     */
+    public Collection<String> getExtList() {
+        return extList;
+    }
+}
---
+++ src/test/java/com/cmayes/common/file/TestExtensionFilter.java
@@ -0,0 +1,47 @@
+package com.cmayes.common.file;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Test different extension filters.
+ * 
+ * @author cmayes
+ */
+public class TestExtensionFilter {
+    private static final String OUT_EXT = ".out";
+    private static final String LOG_EXT = ".log";
+    private static final List<String> EXT_LIST1 = Arrays.asList(LOG_EXT,
+            OUT_EXT);
+
+    @Test
+    public void testIncluded() {
+        ExtensionFilter filter = new ExtensionFilter(EXT_LIST1);
+        assertTrue(filter.accept(null, "whatever.log"));
+        assertFalse(filter.accept(null, "whateverlog"));
+    }
+
+    @Test
+    public void testExcluded() {
+        ExtensionFilter filter = new ExtensionFilter(EXT_LIST1);
+        assertFalse(filter.accept(null, "whatever.txt"));
+    }
+
+    @Test
+    public void testVarargs() {
+        ExtensionFilter filter = new ExtensionFilter(OUT_EXT);
+        assertTrue(filter.accept(null, "whatever.out"));
+        assertFalse(filter.accept(null, "whatever.log"));
+        assertThat(filter.getExtList().size(), equalTo(1));
+        assertThat(filter.getExtList(), hasItem(OUT_EXT));
+    }
+}
